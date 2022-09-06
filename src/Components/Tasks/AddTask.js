bfcd5596cd2549221d43cec7254727aa88829c3a
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-09-06 23:18:51 +0300

    adds files

--- src/Components/Tasks/AddTask.js
+++ src/Components/Tasks/AddTask.js
@@ -25,5 +25,43 @@ const AddTask = ({ onAdd, showAddTask }) => {
     }
 
 
-  
+    return (
+        <>
+            { showAddTask ? 
+                <form className='add-form' onSubmit={onSubmit}>
+                    <div className='form-control'>
+                        <label>Task</label>
+                        <input 
+                            type='text' 
+                            placeholder='Add Task' 
+                            value={text} 
+                            onChange={(e) => setText(e.target.value)} 
+                        />
+                    </div>
+                    <div className='form-control'>
+                        <label>Day & Time</label>
+                        <input 
+                            type='text' 
+                            placeholder='Add Day & Time'
+                            value={day} 
+                            onChange={(e) => setDay(e.target.value)} 
+                        />
+                    </div>
+                    <div className='form-control form-control-check'>
+                        <input 
+                            type='checkbox'
+                            checked={reminder}
+                            value={reminder} 
+                            onChange={(e) => setReminder(e.currentTarget.checked)} 
+                        />
+                        <label>Set Reminder</label>
+                    </div>
+
+                    <input type='submit' value='Save Task' className='btn btn-block' />
+                </form>
+             : '' }
+        </>
+    )
+}
+
 export default AddTask
