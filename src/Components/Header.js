379af4e793dfe0c2b6940aa5ad61f74c061ccb59
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-09-06 19:56:12 +0300

    updates header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,4 +9,31 @@ const Header = ({ title, onAdd, showAdd }) => {
     return (
         <header className='header'>
             <h1>{ title }</h1>
-           
+            {
+                location.pathname === '/' ? 
+                <Button 
+                color={!showAdd ? 'green': 'red'} 
+                text={!showAdd ? 'Add': 'Close'} 
+                onClick={ () => onAdd() } 
+                /> : ''
+            }
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Temp Title'
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired
+}
+
+
+// CSS in JSX
+// const headingStyle = {
+//     color: 'red', 
+//     textAlign: 'center'
+// }
+
+export default Header
